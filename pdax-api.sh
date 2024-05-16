f55ab318c8f438c7349f2f0163bab6e3d03f2053
Man Mai <man.mai@pcomssb.com>
2024-05-16 11:42:49 +0700

    - modify sh cmd

--- pdax-api.sh
+++ pdax-api.sh
@@ -1,36 +1,5 @@
-echo 'changing directory to /api'
-cd api
+echo 'generating files for pdax-api'
+java -jar openapi-generator-cli-6.3.0.jar generate -i pdax-orchestrator-api.yml -g dart-dio -o ./
 
- rm -r pdax-api
-
- echo 'generating files for pdax-api'
- java -jar openapi-generator-cli-6.3.0.jar generate -i pdax-api.yml -g dart-dio -o pdax-api
-
- echo 'changing directory to /api/pdax-api'
- cd pdax-api
-
- echo 'running build_runner and deleting conflicting files...'
-
- fvm flutter pub get
- fvm flutter pub run build_runner build --delete-conflicting-outputs
-
- cd ..
-
-
-#orchestrator
-
-# rm -r pdax-orchestrator-api
-# echo 'generating files for orchestrator'
-# java -jar openapi-generator-cli-6.3.0.jar generate -i pdax-orchestrator-api.yml -g dart-dio -o pdax-orchestrator-api --additional-properties=pubName=openapi_orchestrator
-#
-# echo 'changing directory to /api/pdax-orchestrator-api'
-# cd pdax-orchestrator-api
-#
-# echo 'running build_runner and deleting conflicting files...'
-#
-# fvm flutter pub get
-# fvm flutter pub run build_runner build --delete-conflicting-outputs
-#
-# cd ..
-# cd ..
-# echo 'done'
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
